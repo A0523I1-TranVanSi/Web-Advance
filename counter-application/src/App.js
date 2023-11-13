d09bf2e6e590409e481c7645f8414f64c21722d5
A0523I1-TranVanSi <sitran.120503@gmail.com>
2023-11-13 15:38:46 +0700

    E04

---
+++ counter-application/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CounterRow from "./components/CounterRow";
+import ProductRow from "./components/ProductRow.js";
+import ProductTable from "./components/ProductTable";
+import ProductCategoryRow from "./components/ProductCategoryRow";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <>
+      <header>
+      <SearchBar/>
+      </header>
+      <div id="counterContainer">
+        <ProductTable/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ counter-application/src/components/ProductCategoryRow.js
@@ -0,0 +1,11 @@
+function ProductCategoryRow(props) {
+
+    return (
+<>
+<td colspan="2" class="content">Fruits</td>
+</>
+
+    );
+}
+
+export default ProductCategoryRow;
